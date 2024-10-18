1d6088417feb9f1f07a571356fc1db284273b290
codeUser2000 <aidahakobyanf12@gmail.com>
2024-10-18 23:13:42 +0400

    port change

--- ats/src/config.js
+++ ats/src/config.js
@@ -2,4 +2,4 @@ const isHTTPS = window.location.protocol === "https:";
 
 export const REACT_APP_API_URL = isHTTPS
     ? 'https://ats.am'
-    : 'http://10.227.0.86:4000';
+    : 'http://10.227.0.86:4003';
