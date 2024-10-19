c24c87bd801c65178542db79731fa8aa3078f421
codeUser2000 <aidahakobyanf12@gmail.com>
2024-10-19 17:28:39 +0400

    ats register

--- ats/src/page/Register.js
+++ ats/src/page/Register.js
@@ -155,38 +155,65 @@ function Register(props) {
                     </p>
 
                 </div>
-                <div className="login_block">
+                <div className="login_block_input">
                     <div>
-                        <div className="login_block_input">
-                            <div>
-                                <label>
-                                    <img src={email} alt=""/>
-                                    <InputMask
-                                        mask="374 (99) 999999"
-                                        maskChar="_"
-                                        placeholder={buttons.email[lang[params?.lang || 'en'] || 1]}
-                                        onChange={(ev) => setForm(ev.target.value)}
-                                        value={form}
-                                    >
-                                        {(inputProps) => <input {...inputProps} />}
-                                    </InputMask>
-                                </label>
-                            </div>
-                            <div>
-                                <button onClick={handleRegister} className="register_btn">
-                                    <img src={plus} alt=''/>
-                                    {buttons.reg[lang[params?.lang || 'en'] || 1]}
-                                </button>
-                            </div>
-
-                        </div>
+                        <label>
+                            <img width="17" height="17" src={company} alt=""/>
+                            <input placeholder={input.company[lang[params?.lang || 'en']]}
+                                   onChange={(ev) => handleChange('company', ev.target.value)}
+                                   value={form.company}/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <img src={email} alt=""/>
+                            <input placeholder={input.name[lang[params?.lang || 'en']]}
+                                   onChange={(ev) => handleChange('fullName', ev.target.value)}
+                                   value={form.fullName}/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <img width="17" height="17" src={department} alt=""/>
+                            <input placeholder={input.position[lang[params?.lang || 'en']]}
+                                   onChange={(ev) => handleChange('position', ev.target.value)}
+                                   value={form.position}/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <img width="17" height="17" src={mail} alt=""/>
+                            <input placeholder={input.email[lang[params?.lang || 'en']]}
+                                   onChange={(ev) => handleChange('email', ev.target.value)}
+                                   value={form.email}/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <PhoneInput
+                                international
+                                defaultCountry="US"
+                                placeholder={buttons.email[lang[params?.lang || 'en']]}
+                                value={form.phone}
+                                onChange={(ev) => handleChange('phone',ev)}/>
+                        </label>
+                    </div>
+                    <div>
+                        <p>{log_reg.desc[lang[params?.lang || 'en'] || 1]}</p>
+                    </div>
+                    <div className="login_block_input">
                         <div>
-                            <p className="login_block_text">{log_reg.request[lang[params?.lang || 'en'] || 1]}</p>
-
+                            <button onClick={handleRegister} className="register_btn">
+                                <img src={plus} alt=''/>
+                                {buttons.reg[lang[params?.lang || 'en'] || 1]}
+                            </button>
                         </div>
+
                     </div>
 
+
                 </div>
+
             </div>
 
         )
